a5985cba92596f44f1ccf2b5b2355ddeb04b9980
naveenkumar <naveenkumarsk31@gmail.com>
2023-10-27 16:50:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>this is a test app</h1>
+     <p>built using react</p>
+     <p>author:yashu</p>
+    </div>
+  );
+}
+
+export default App;
